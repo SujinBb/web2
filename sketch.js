f6d5c9443fb1144503212e28580eedbdf18f9a48
SujinBb <36348220+SujinBb@users.noreply.github.com>
2019-08-04 02:14:07 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,9 @@
-let hand;
+et hand;
 let spritesheet;
 let spritedata;
 let animation =[];
-
+let x =400;
+let y =400;
 
 
 function preload(){
@@ -19,26 +20,25 @@ createCanvas (1024,540);
   animation.push(img);
  }
 
- //hand = new Sprite (animation,100,100,1 );
 console.log(animation);
 }
 
 function draw() {
 background(255);
+
 let index = floor (frameCount / 4) % animation.length;
 image(animation[index],0,150,320,240);
+  
+  if (x >= 400){
+   x = 0; 
+  }
 }
 
-  
-this.dir = function (x, y) {
-    this.xspeed = x;
-    this.yspeed = y;
-  }
-
 function keyPressed() {
- if (keyCode === RIGHT_ARROW) {
-    hand.dir(1, 0);
-  } else if (keyCode === LEFT_ARROW) {
-    hand.dir(-1, 0);
+ 
+  if (keyCode === LEFT_ARROW) {
+    x = x - 5;
+  } else if (keyCode === RIGHT_ARROW) {
+    x = x + 5;
   }
 }
