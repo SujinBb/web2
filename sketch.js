21bdaabd9aac9f6ea11e4336bc2ae903463a1935
SujinBb <36348220+SujinBb@users.noreply.github.com>
2019-08-04 03:03:52 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -36,8 +36,8 @@ image(animation[index],x-200,y-50,320,240);
 
 function keyPressed() {
   if (keyCode === LEFT_ARROW) {
-    x = x - 10;
+    x = x - 20;
   } else if (keyCode === RIGHT_ARROW) {
-    x = x + 10;
+    x = x + 20;
   }
 }
