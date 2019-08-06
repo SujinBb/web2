d3b7dee4b51721e9de844367b992fae6ce9b49a2
SujinBb <36348220+SujinBb@users.noreply.github.com>
2019-08-06 02:40:39 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,8 +5,6 @@ let animation =[];
 let x =0;
 let y =250;
 
-
-
 function preload(){
 spritedata = loadJSON('handsp.json');
 spritesheet = loadImage('handsp.png');
@@ -21,33 +19,32 @@ let img = spritesheet.get(pos.x,pos.y,pos.w,pos.h);
 animation.push(img);
 }
 console.log(animation);
-
-let this.r = 100;
-let this.x = 50;
-let this.y = height - this.r;
-let this.vy = 0;
-let this.gravity = 3;
-  
-jump() {
-      if (this.y == height - this.r) {
-        this.vy = -35;
-      }
-    }
-move() {
-      this.y += this.vy;
-      this.vy += this.gravity;
-      this.y = constrain(this.y, 0, height - this.r);
-    }
 }
 
 function draw() {
 background(255);
-
 let index = floor (frameCount / 6) % animation.length;
 image(animation[index],x,y,300,240);
-
 }
 
+function Hand(){
+  let this.r = 100;
+  let this.x = 50;
+  let this.y = height - this.r;
+  let this.vy = 0;
+  let this.gravity = 3;
+
+  this.jump = function() {
+        if (this.y == height - this.r) {
+          this.vy = -35;
+        }
+      }
+  this.move= function() {
+        this.y += this.vy;
+        this.vy += this.gravity;
+        this.y = constrain(this.y, 0, height - this.r);
+  }
+}
 
 function keyPressed() {
 if (keyCode === LEFT_ARROW) {
@@ -56,5 +53,5 @@ if (keyCode === LEFT_ARROW) {
  x = x + 30;
  else if (key == ' ') {
   hand.jump();
-}
+ }
 }
