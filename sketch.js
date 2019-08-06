84c4aa19fe1f71ac127c1604c37169c56b03244c
SujinBb <36348220+SujinBb@users.noreply.github.com>
2019-08-06 02:28:57 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,21 +6,38 @@ let x =0;
 let y =250;
 
 
+
 function preload(){
-  spritedata = loadJSON('handsp.json');
-  spritesheet = loadImage('handsp.png');
+spritedata = loadJSON('handsp.json');
+spritesheet = loadImage('handsp.png');
 }
 
 function setup() {
 createCanvas (1400,700);
- let frames = spritedata.frames;
- for (let i = 0; i< frames.length; i++){
-  let pos= frames [i].position;
-  let img = spritesheet.get(pos.x,pos.y,pos.w,pos.h);
-  animation.push(img);
- }
-
+let frames = spritedata.frames;
+for (let i = 0; i< frames.length; i++){
+let pos= frames [i].position;
+let img = spritesheet.get(pos.x,pos.y,pos.w,pos.h);
+animation.push(img);
+}
 console.log(animation);
+
+let this.r = 100;
+let this.x = 50;
+let this.y = height - this.r;
+let this.vy = 0;
+let this.gravity = 3;
+  
+jump() {
+      if (this.y == height - this.r) {
+        this.vy = -35;
+      }
+    }
+move() {
+      this.y += this.vy;
+      this.vy += this.gravity;
+      this.y = constrain(this.y, 0, height - this.r);
+    }
 }
 
 function draw() {
@@ -28,16 +45,16 @@ background(255);
 
 let index = floor (frameCount / 6) % animation.length;
 image(animation[index],x,y,300,240);
-  
-  //if (x >= 400){
-  // x = 0; 
-  //}
+
 }
 
+
 function keyPressed() {
-  if (keyCode === LEFT_ARROW) {
-    x = x - 30;
-  } else if (keyCode === RIGHT_ARROW) {
-    x = x + 30;
-  }
+if (keyCode === LEFT_ARROW) {
+ x = x - 30;
+} else if (keyCode === RIGHT_ARROW) {
+ x = x + 30;
+ else if (key == ' ') {
+  hand.jump();
+}
 }
