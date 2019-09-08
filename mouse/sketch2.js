0e59d5f12cc8f1bc1814a5d24f7e10aed181afb7
SujinBb <36348220+SujinBb@users.noreply.github.com>
2019-09-08 09:05:30 +0200

    Add files via upload

---
+++ mouse/sketch2.js
@@ -0,0 +1,17 @@
+var gif_createImg;
+
+function preload() {
+  gif_createImg = createImg("manipul2.gif");
+}
+
+function setup() {
+  createCanvas(1400,700);
+  background(255);
+   textSize(50);
+ text("DANGER",540,100);
+}
+
+function draw() {
+gif_createImg.position(mouseX, mouseY - 66, mouseX, mouseY + 66);
+}
+
