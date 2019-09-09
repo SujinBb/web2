81d9f02099239ecd0df5810256d96ab14133ed6e
SujinBb <36348220+SujinBb@users.noreply.github.com>
2019-09-09 03:49:31 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@
 	let y = 250;
         let myText = [];
         let randomLineOfText;
-        let link;
+        //let link;
       
 	function preload(){
          myText = loadStrings('dead.txt'); 
@@ -29,7 +29,6 @@
 	  let pos= frames [i].position;
 	  let img = spritesheet.get(pos.x,pos.y,pos.w,pos.h);
 	  animation.push(img);
-       hand.mouseClicked(link); 
 	 } 
 	}
 
@@ -37,16 +36,11 @@
 	//background(r,g,b);
 	fill(0);
         textSize(150);
-        //link =
 	text(myText[randomLineOfText],width/2,height/2);
 	let index = floor (frameCount / 6) % animation.length;
 	image(animation[index],x,y,300,240);
 	}
 
-
-       function link() {
-        text(window.open('mouse/manipul.html'),'myText[randomLineOfText]', width/2, height/2);
-          }
 
 	function keyPressed() {
 	  if (keyCode === LEFT_ARROW) {
