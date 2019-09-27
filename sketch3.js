3b890b1af88dede8b37d97b1ee9895a610385d34
SujinBb <36348220+SujinBb@users.noreply.github.com>
2019-09-28 01:01:28 +0200

    Update sketch3.js

--- sketch3.js
+++ sketch3.js
@@ -1,17 +1,33 @@
-   
 var gif_createImg;
+var myText = [];
+var randomLineOfText;
 
 function preload() {
-  gif_createImg = createImg("manipul2.gif");
+    myText = loadStrings('atikel.txt'); 
+  gif_createImg = createImg("manipul3.gif");
 }
 
 function setup() {
   createCanvas(1400,700);
   background(255);
-   textSize(50);
- text("DANGER",540,100);
+  randomLineOfText = round(random(0,myText.length));
+  print(myText[randomLineOfText]);
+  textAlign(CENTER,CENTER);
+
 }
 
 function draw() {
-gif_createImg.position(mouseX, mouseY - 66, mouseX, mouseY + 66);
+ gif_createImg.position(mouseX, mouseY - 66, mouseX, mouseY + 66);
+ textSize(40);
+	text(myText[randomLineOfText],width/2,height/2);
+}
+
+function mousePressed(){
+ background(255);
+  r = random(255);
+ g = random(255);
+ b = random(255);
+  fill(r,g,b);
+randomLineOfText = round(random(0,myText.length));
+  console.log(randomLineOfText);
 }
